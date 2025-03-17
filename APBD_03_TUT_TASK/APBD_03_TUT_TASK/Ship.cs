ff754639df855233d5671b15ed08c985cf7af0ed
JosephJostaaa <s30209@pjwstk.edu.pl>
2025-03-17 17:03:58 +0100

    Added something to the Ship class

--- APBD_03_TUT_TASK/APBD_03_TUT_TASK/Ship.cs
+++ APBD_03_TUT_TASK/APBD_03_TUT_TASK/Ship.cs
@@ -4,8 +4,15 @@ public class Ship
 {
     public List<Container> Containers { get; } = new List<Container>();
     public double MaxSpeed { get; set; }
-    public double MaxNumberOfContainers { get; set; }
+    public int MaxNumberOfContainers { get; set; }
     public double MaxWeightOfContainers { get; set; }
+
+    public void LoadContainer(Container container)
+    {
+        Containers.Add(container);
+    }
+    
+    
     
     
 }
